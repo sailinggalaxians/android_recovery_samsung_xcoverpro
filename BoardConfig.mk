1df6a1faca4e26df6cd079a85ebb0ebcda4e66da
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2025-04-10 02:49:04 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 DEVICE_PATH := device/samsung/xcoverpro
 
 # For building with minimal manifest
